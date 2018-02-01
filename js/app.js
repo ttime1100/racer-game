5fee147ceeb7f83171f4873129c2f1e2bb698b24
ttime1100 <ttime1100@gmail.com>
2018-02-01 10:41:59 -0800

    design, programming, question temp

--- js/app.js
+++ js/app.js
@@ -2,20 +2,58 @@ $(document).ready( function () {
 
 
 
-$("button").mouseenter( function() {
+$("#press").mouseenter( function() {
 
 $(this).animate({height:'+=100px'});
 });
 
-$("button").mouseleave( function() {
-  $(this).animate({height:"-=100px"});
+$("#press").mouseleave( function() {
+  $(this).animate({height:'-=100px'});
 });
 
-$("button").click( function() {
+$("#press").click( function() {
 document.location.href = "../views/game.html"
 
 });
 
+$("#go").click(function() {
+  $("#beev").animate({ left:'+=20px' });
+    x++;
+
+    if ( x === 100) {
+    alert("You've Won");
+    document.location.href = "../views/game.html"
+  }
+  });
+
+
+$("#go-three").click(function() {
+  $("#bugs").animate({ left:'+=20px' });
+
+});
+
+$("#go-two").click(function() {
+  $("#scooby").animate({ left:'+=20px' });
+});
+
+$("body").on('keypress', function(e) {
+if(e.charCode === 112) {
+  $("#beev").animate({left: '+=20px'});
+}
+  });
+
+$("body").on('keypress', function(e) {
+if(e.charCode === 104) {
+  $("#scooby").animate({left: '+=20px'});
+}
+  });
+
+  $("body").on('keypress', function(e) {
+if (e.charCode === 113) {
+  $("#bugs").animate({left: '+=20px'})
+}
+
+  });
 
 
 });
